d7462596d93872c5b051a71e981c4ad5add2b48f
Sukhov Artyom <strangecodder@gmail.com>
2024-03-08 22:16:59 +0300

    develop | 08.03.2024 | Added repository for entity Task

---
+++ src/main/java/com/example/kanban/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.example.kanban.repository;
+
+import com.example.kanban.entity.Task;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task,Integer> {
+}
